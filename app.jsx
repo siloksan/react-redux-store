b0cae0818a2b91af4f589a9608d5fadee4f499a3
siloksan <tutel07@gmail.com>
2023-10-03 20:49:29 +0300

    added styles

--- app.jsx
+++ app.jsx
@@ -1,17 +1,22 @@
 import {Route, Routes} from "react-router-dom";
 import {CartPage, HomePage} from "../pages";
+import Header from "../header/header";
+
+import "./app.css"
 
 const App = () => {
 
 	return (
 		<div>
-			Heda
-			<Routes>
-				<Route path="/" element={<HomePage />} />
-				<Route path="/cart" element={<CartPage />} />
-				App
-				{/*<ErrorButton />*/}
-			</Routes>
+			<Header />
+			<div className="container-sm main">
+				<Routes>
+					<Route path="/" element={<HomePage />} />
+					<Route path="/cart" element={<CartPage />} />
+					App
+					{/*<ErrorButton />*/}
+				</Routes>
+			</div>
 		</div>
 	);
 }
