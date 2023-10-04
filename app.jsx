5c81f27d053ef74e72813f472b5586a42b0afa82
siloksan <tutel07@gmail.com>
2023-10-04 12:29:26 +0300

    git clean

--- app.jsx
+++ app.jsx
@@ -3,19 +3,21 @@ import {CartPage, HomePage} from "../pages";
 import Header from "../header/header";
 
 import "./app.css"
+import ShoppingCartTable from "../shopping-cart-table/shopping-cart-table";
 
 const App = () => {
 
 	return (
 		<div>
-			<Header />
-			<div className="container-sm main">
+			<Header numItems={5} total={250}/>
+			<div className="container-sm main fl-d">
 				<Routes>
 					<Route path="/" element={<HomePage />} />
 					<Route path="/cart" element={<CartPage />} />
 					App
 					{/*<ErrorButton />*/}
 				</Routes>
+				<ShoppingCartTable total={120}/>
 			</div>
 		</div>
 	);
